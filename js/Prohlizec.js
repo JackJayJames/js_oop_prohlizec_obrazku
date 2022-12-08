4dc380a12ca7045d852d27f80d6455aa135cd60b
JackJayJames <ciklops@seznam.cz>
2022-12-08 12:19:14 +0100

    obrazky do třídy a onclick
    obrazky byly přídany v konstruktoru třídy Prohlizec, a vlozen onclick pro každý obrázek

--- js/Prohlizec.js
+++ js/Prohlizec.js
@@ -2,6 +2,19 @@
 
 class Prohlizec{
     constructor(){
-        
+        this._obrazky = document.querySelectorAll(".obrazky");
+
+        this._onclickObrazky();
+        this.vypis();
+    }
+    vypis(){
+        console.log(this._obrazky);
+    }
+    _onclickObrazky(){
+        for(const obrazek of this._obrazky){
+            obrazek.onclick = () => {
+                console.log(obrazek.alt);
+            };
+        }
     }
 }
